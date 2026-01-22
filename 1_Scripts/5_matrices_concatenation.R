c7481524e098fa74608f7109051732760dfbc34a
Jorge-LGH <jorgel.goh@gmail.com>
2026-01-22 12:46:59 -0600

    Satrting

---
+++ 1_Scripts/5_matrices_concatenation.R
@@ -0,0 +1,10 @@
+# This script is intended as a concatenation for the selected omic data types. In this case, they are
+# miRNA, mRNA, and CpG sites. This concatenation of data types is based on the SGCCA method.
+
+#-----------------------Load libraries-----------------------
+
+#-----------------------Load data----------------------------
+# Load only the blocks which have been normalized, scaled, and centered
+exp_data <- read.table("3_Data/eig_exp_comparable.tsv",sep=',',row.names=T)     # mRNA expression data
+mir_data <- read.table("3_Data/eig_mir_comparable.tsv",sep=',',row.names=T)     # miRNA expression data
+                                                                                # CpG methylation data
--- 1_Scripts/5_matrices_standardization.R
+++
@@ -1,2 +0,0 @@
-# This script is meant as a guide for the matrices standardization. Since every matrix contains
-# different and redundant informatio, the MFA approach is slected since 
